43faa69c7015d2b7b39e3393e0f82a7e9ba66b49
bunyaminkalkan <bnkalkan41@gmail.com>
2025-10-16 09:12:35 +0300

    feat: add exception middleware and it extension

---
+++ src/Shared/ECommerce.BuildingBlocks.Shared.Kernel/Extensions/ExceptionMiddlewareExtension.cs
@@ -0,0 +1,12 @@
+﻿using ECommerce.BuildingBlocks.Shared.Kernel.Middlewares;
+using Microsoft.AspNetCore.Builder;
+
+namespace ECommerce.BuildingBlocks.Shared.Kernel.Extensions;
+
+public static class ExceptionMiddlewareExtension
+{
+    public static IApplicationBuilder UseExceptionMiddleware(IApplicationBuilder app)
+    {
+        return app.UseMiddleware<ExceptionMiddleware>();
+    }
+}
