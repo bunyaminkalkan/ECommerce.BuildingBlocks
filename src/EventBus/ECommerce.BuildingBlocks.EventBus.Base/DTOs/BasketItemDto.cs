f2ce31f29c8abd380fdeda3f69a0487c3ad4130d
bunyaminkalkan <bnkalkan41@gmail.com>
2025-10-23 13:59:41 +0300

    fix: fix basket checkout event

--- src/EventBus/ECommerce.BuildingBlocks.EventBus.Base/DTOs/BasketItemDto.cs
+++ src/EventBus/ECommerce.BuildingBlocks.EventBus.Base/DTOs/BasketItemDto.cs
@@ -1,12 +1,7 @@
-﻿using ECommerce.BuildingBlocks.Shared.Kernel.ValueObjects;
-
-namespace ECommerce.BuildingBlocks.EventBus.Base.DTOs;
+﻿namespace ECommerce.BuildingBlocks.EventBus.Base.DTOs;
 
 public class BasketItemDto
 {
     public Guid ProductId { get; set; }
-    public string ProductName { get; set; }
-    public Money UnitPrice { get; set; }
     public int Quantity { get; set; }
-    public string ImageUrl { get; set; }
 }
--- src/EventBus/ECommerce.BuildingBlocks.EventBus.Base/Events/Baskets/BasketCheckedOutIntegrationEvent.cs
+++ src/EventBus/ECommerce.BuildingBlocks.EventBus.Base/Events/Baskets/BasketCheckedOutIntegrationEvent.cs
@@ -8,15 +8,12 @@ public class BasketCheckedOutIntegrationEvent : IntegrationEvent
     public DateTime OccurredOn { get; set; }
     public Guid CorrelationId { get; set; }
 
-    public Guid BasketId { get; set; }
     public Guid CustomerId { get; set; }
     public string CustomerEmail { get; set; }
     public string CustomerName { get; set; }
 
     public List<BasketItemDto> Items { get; set; }
-    public Money TotalAmount { get; set; }
 
     public Address ShippingAddress { get; set; }
     public Address BillingAddress { get; set; }
-    public string PaymentMethod { get; set; }
 }
