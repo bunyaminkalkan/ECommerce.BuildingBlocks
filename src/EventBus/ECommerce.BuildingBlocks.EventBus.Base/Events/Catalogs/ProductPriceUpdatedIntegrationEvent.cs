829bd479c452563824561a194a94a8d66871c640
bunyaminkalkan <bnkalkan41@gmail.com>
2025-10-24 16:24:18 +0300

    feat: add product price update and stock update events

---
+++ src/EventBus/ECommerce.BuildingBlocks.EventBus.Base/Events/Catalogs/ProductPriceUpdatedIntegrationEvent.cs
@@ -0,0 +1,17 @@
+﻿using ECommerce.BuildingBlocks.Shared.Kernel.ValueObjects;
+
+namespace ECommerce.BuildingBlocks.EventBus.Base.Events.Catalogs;
+
+public class ProductPriceUpdatedIntegrationEvent : IntegrationEvent
+{
+    public DateTime OccurredOn { get; set; }
+    public Guid ProductId { get; set; }
+    public Money OldPrice { get; set; }
+    public Money NewPrice { get; set; }
+    public ProductPriceUpdatedIntegrationEvent(Guid productId, Money oldPrice, Money newPrice)
+    {
+        ProductId = productId;
+        OldPrice = oldPrice;
+        NewPrice = newPrice;
+    }
+}
---
+++ src/EventBus/ECommerce.BuildingBlocks.EventBus.Base/Events/Inventories/StockUpdatedIntegrationEvent.cs
@@ -0,0 +1,13 @@
+﻿namespace ECommerce.BuildingBlocks.EventBus.Base.Events.Inventories;
+
+public class StockUpdatedIntegrationEvent : IntegrationEvent
+{
+    public DateTime OccurredOn { get; set; }
+    public Guid ProductId { get; set; }
+    public int QuantityChange { get; set; }
+    public StockUpdatedIntegrationEvent(Guid productId, int quantityChange)
+    {
+        ProductId = productId;
+        QuantityChange = quantityChange;
+    }
+}
